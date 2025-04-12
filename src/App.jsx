a7c345e8f9d42f47d969c72850492654e194742b
Avindi Obeyesekere <avindiobeyesekere1@gmail.com>
2025-04-12 12:07:01 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Navbar } from './components/Navbar';
+import { Dashbord } from './components/Dashbord';
+import { Features } from './components/Features';
+import { HowItWorks } from './components/HowItWorks';
+import { Description } from './components/Description';
+import { Footer } from './components/Footer';
+
+export default function App() {
+  return (
+    <div className="w-full min-h-screen bg-white">
+      <Navbar />
+      <Dashbord />
+      <Features />
+      <HowItWorks />
+      <Description />
+      <Footer />
+    </div>
+  );
+}
